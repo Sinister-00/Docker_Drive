cf114ba4f68ca02c2b1f463433fc440777bdb9d4
Swapnil Dhamu <swapnildhamu@Swapnils-MacBook-Pro.local>
2023-12-30 07:17:51 +0530

    Added notes
    Signed-off-by: Swapnil Dhamu <swapnildhamu@Swapnils-MacBook-Pro.local>

--- api/app.js
+++ api/app.js
@@ -9,7 +9,7 @@ app.get("/", (req, res) => {
   res.json([
     {
       id: "1",
-      title: "Book Review: The Bear & The Nightingale...ABCddff",
+      title: "Book Review: The Bear & The Nightingale...pushed_image_check",
     },
     {
       id: "2",
